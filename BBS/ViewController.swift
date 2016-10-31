47ce733e37b73458ab640be83308b5b3f86d5732
kazuminn <e145702@ie.u-ryukyu.ac.jp>
2016-10-31 19:34:55 +0900

    first commit

---
+++ BBS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BBS
+//
+//  Created by 木下　和巳 on 10/31/16.
+//  Copyright © 2016 programing4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
